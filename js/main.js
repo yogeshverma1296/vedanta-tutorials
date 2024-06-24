40bca343e9cd5c5e80f73bfa630719ba4bafe6da
yogesh <yogeshverma1296@gmail.com>
2024-06-24 21:48:58 +0530

    Version 1.1 -> Index.html editted

--- js/main.js
+++ js/main.js
@@ -27,17 +27,17 @@
     
     
     // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
+    // $(window).scroll(function () {
+    //     if ($(this).scrollTop() > 300) {
+    //         $('.back-to-top').fadeIn('slow');
+    //     } else {
+    //         $('.back-to-top').fadeOut('slow');
+    //     }
+    // });
+    // $('.back-to-top').click(function () {
+    //     $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+    //     return false;
+    // });
 
 
     // Testimonials carousel
@@ -52,3 +52,32 @@
     
 })(jQuery);
 
+
+
+// my js
+// (function(){
+//     emailjs.init('template_s5knp4u'); // Replace 'YOUR_USER_ID' with your actual EmailJS user ID
+// })();
+
+// function sendEmail(event) {
+//     event.preventDefault(); // Prevent the default form submission
+    
+//     // Collect form data
+//     var formData = {
+//         your_name: document.getElementById('gname').value,
+//         email: document.getElementById('gmail').value,
+//         course_name: document.getElementById('cname').value,
+//         mobile_number: document.getElementById('number').value,
+//         message: document.getElementById('message').value
+//     };
+    
+//     // Send email
+//     emailjs.send('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', formData)
+//     .then(function(response) {
+//         console.log('SUCCESS!', response.status, response.text);
+//         alert('Your appointment request has been sent successfully.');
+//     }, function(error) {
+//         console.log('FAILED...', error);
+//         alert('There was an error sending your appointment request. Please try again.');
+//     });
+// }
